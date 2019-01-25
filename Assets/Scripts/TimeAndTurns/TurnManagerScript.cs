2b10c8aa69a83e151f380fd204533473763e1ad7
Jacek Staszewski <jaceks@cherrypickgames.com>
2019-01-25 23:04:30 +0100

    Turn Manager stub

---
+++ Assets/Scripts/TimeAndTurns/TurnManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polyjam2019
+{
+    public class TurnManagerScript : MonoBehaviour
+    {
+        private TimeManagerScript timeManager;
+
+        public void EndTurn()
+        {
+            if(timeManager != null)
+            {
+                timeManager.FireOnDayPassed();
+            }
+        }
+
+        private void Awake()
+        {
+            // set timeManager with object from singleton
+        }
+    }
+}
