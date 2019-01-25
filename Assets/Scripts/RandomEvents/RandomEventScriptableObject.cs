520030fb23bd5e791f75d26641c6efd95d0c1237
Jacek Staszewski <jaceks@cherrypickgames.com>
2019-01-26 00:07:28 +0100

    RandomEvents scriptable objects stub

---
+++ Assets/Scripts/RandomEvents/RandomEventScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polyjam2019
+{
+    public class RandomEventScriptableObject : ScriptableObject
+    {
+        [SerializeField]
+        private string plotDescription;
+
+        public string PlotDescription { get { return plotDescription; } }
+    }
+}
