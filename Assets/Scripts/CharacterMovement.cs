df123b300bd34de5f60f93b7eb4a22c829f6380f
Rafał Tyl <rafal.tyl@silverbullet.pl>
2019-01-26 13:24:25 +0100

    First movement prototype

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float velocityChangeSpeed = 1.0f; //TODO: add Range attribute
+	[SerializeField] private float maxSpeed = 2.0f;
+	
+	private new Rigidbody2D rigidbody2D;
+	private Vector2 targetVelocity;
+	
+	/// <summary>
+	/// Sets target direction and speed.
+	/// </summary>
+	/// <param name="direction">Desired movement direction</param>
+	/// <param name="speedPercentage">A value from [0,1] range, determining target speed</param>
+	public void Move(Vector2 direction, float speedPercentage)
+	{
+		speedPercentage = Mathf.Clamp01(speedPercentage);
+		targetVelocity = direction.normalized * speedPercentage * maxSpeed;
+	}
+	
+	/// <summary>
+	/// Stops movement gradually
+	/// </summary>
+	public void Stop()
+	{
+		targetVelocity = Vector2.zero;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	/// <summary>
+	/// Stops movement immediately, disregarding inertia
+	/// </summary>
+	public void StopImmediate()
+	{
+		targetVelocity = rigidbody2D.velocity = Vector2.zero;
+	}
+	
+	private void Awake()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private const float minSpeedToRotate = 0.001f;
+	private void FixedUpdate()
+	{
+		rigidbody2D.velocity = Vector2.MoveTowards(rigidbody2D.velocity, targetVelocity, velocityChangeSpeed * Time.fixedDeltaTime);
+		if (rigidbody2D.velocity.magnitude > minSpeedToRotate)
+		{
+			Vector2 movementDir = rigidbody2D.velocity.normalized;
+			rigidbody2D.rotation = Mathf.Atan2(movementDir.y, movementDir.x) * Mathf.Rad2Deg - 90;
+		}
+	}
 }
--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterMovement))]
 public class PlayerCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float minDistanceToCursor = 0.1f;
+	[SerializeField] private float maxDistanceToCursor = 2f;
+	[SerializeField] private float distanceFalloffExponent = 1.0f;
+	
+	private CharacterMovement movement;
+	
+	private void Awake()
+	{
+		movement = GetComponent<CharacterMovement>();
+		distanceFalloffExponent = Mathf.Max(0, distanceFalloffExponent);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Vector2 toCursor = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).XY();
+			float cursorDistance = toCursor.magnitude;
+			float normalizedDistance = Mathf.Clamp01((cursorDistance - minDistanceToCursor) / (maxDistanceToCursor - minDistanceToCursor));
+			float speedPercentage = Mathf.Pow(normalizedDistance, distanceFalloffExponent);
+			movement.Move(toCursor.normalized, speedPercentage);
+		}
+		else
+		{
+			movement.Stop();
+		}
+	}
 }
