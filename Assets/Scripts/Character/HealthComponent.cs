a74275b131384f9d4a22aec66f127bf8f9ed6d62
Rafał Tyl <rafal.tyl@silverbullet.pl>
2019-01-26 17:54:11 +0100

    Import enemy sprites, implement health component

---
+++ Assets/Scripts/Character/HealthComponent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+	[SerializeField] private int maxValue;
+	private int currentValue;
+
+	public int CurrentValue
+	{
+		get => currentValue;
+
+		private set
+		{
+			currentValue = value;
+			OnValueChanged?.Invoke(currentValue);
+			if (currentValue == 0)
+			{
+				OnDeath?.Invoke();
+			}
+		}
+	}
+	
+	public event System.Action OnDeath;
+	public event System.Action<int> OnValueChanged;
+	public event System.Action<int> OnDamageReceived;
+
+	public void Reset()
+	{
+		CurrentValue = maxValue;
+	}
+
+	public void ReceiveDamage(int damage)
+	{
+		CurrentValue -= damage;
+		OnDamageReceived?.Invoke(damage);
+	}
+
+	private void Awake()
+	{
+		Reset();
+	}
+}
