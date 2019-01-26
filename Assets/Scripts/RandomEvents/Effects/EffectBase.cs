48e44744a4504448081745f6dcbe319be6c0fa7e
Jacek Staszewski <jaceks@cherrypickgames.com>
2019-01-26 11:50:17 +0100

    Random Events stubs

---
+++ Assets/Scripts/RandomEvents/Effects/EffectBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Polyjam2019
+{
+    public abstract class EffectBase : ScriptableObject
+    {
+        public abstract void ExecuteEffect(GameObject gameObject);
+    }
+}
--- Assets/Scripts/RandomEvents/RandomEventScriptableObject.cs
+++ Assets/Scripts/RandomEvents/RandomEventScriptableObject.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 namespace Polyjam2019
 {
+    [CreateAssetMenu(fileName = "RandomEvent", menuName = "Random Events/Random Event")]
     public class RandomEventScriptableObject : ScriptableObject
     {
         [SerializeField]
         private string plotDescription;
+
+        [SerializeField]
+        private List<RandomEventChoiceScriptableObject> choices;
 
         public string PlotDescription { get { return plotDescription; } }
     }
