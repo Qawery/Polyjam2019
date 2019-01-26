350d1c8c071eda6ea8bbe23057c74444bc3c8d1d
Rafał Tyl <rafal.tyl@silverbullet.pl>
2019-01-26 19:48:42 +0100

    Import A* plugin

---
+++ Assets/Scripts/Debug/ForceUpwardRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+public class ForceUpwardRotation : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    
+    public void LateUpdate()
+    {
+        Vector3 newPosition = target.position - Vector3.forward * 10;
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+}
