ee2ac04d014b76e10e4f7b12e89867e693607a75
Rafał Tyl <rafal.tyl@silverbullet.pl>
2019-01-27 14:44:23 +0100

    Death

---
+++ Assets/Scripts/SpawnOnDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthComponent))]
+public class SpawnOnDeath : MonoBehaviour
+{
+	[SerializeField] private GameObject[] spawnedObjects;
+	
+	private void Awake()
+	{
+		GetComponent<HealthComponent>().OnDeath += OnDeath;
+	}
+
+	private void OnDeath()
+	{
+		foreach (var obj in spawnedObjects)
+		{
+			Instantiate(obj, transform.position, Quaternion.identity);
+		}
+	}
+}
